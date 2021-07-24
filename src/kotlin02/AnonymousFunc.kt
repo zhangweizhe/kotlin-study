5db82765a356b60e5ead566fcf75fc4c752b6105
张伟哲 <704052130@qq.com>
2021-07-24 15:27:14 +0800

    1、函数作为另一个函数的参数

--- src/kotlin02/AnonymousFunc.kt
+++ src/kotlin02/AnonymousFunc.kt
@@ -55,4 +55,33 @@ fun main() {
     }
     println(af4("Jack", 2020)) //Hello Jack from 2020
 
+
+    // 函数作为另一个函数的参数
+    val getDiscount1 = {goodsName: String, hour: Int->
+        val year = 2021
+        "${year}年双十一，${goodsName}促销还有 ${hour} 小时"
+    }
+
+    showOnBoard("游戏机", getDiscount1, 7.8f) //2021年双十一，游戏机促销还有 4 小时
+
+    //2022年双十一，笔记本电脑促销还有 8 分钟
+    showOnBoard("笔记本电脑", { gn:String, hour: Int ->
+        "2022年双十一，${gn}促销还有 $hour 分钟"
+    }, 6.5f)
+
+    // 如果函数作为最后一个参数，调用时作为参数的函数体，写在()之外
+    //2022年双十一，显卡促销还有 21 分钟
+    showOnBoard("显卡") { gn:String, hour: Int ->
+        "2022年双十一，${gn}促销还有 $hour 分钟"
+    }
+}
+
+fun showOnBoard(goodsName: String, getDiscount: (String, Int) -> String, discount:Float) {
+    val hour = (1..24).shuffled().last()
+    println(getDiscount(goodsName,  hour))
+}
+
+fun showOnBoard(goodsName: String, getDiscount: (String, Int) -> String) {
+    val hour = (1..24).shuffled().last()
+    println(getDiscount(goodsName,  hour))
 }
