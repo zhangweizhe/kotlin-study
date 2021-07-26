cbecd6fb14a8110bf193b1d4fbc779454aa5ff05
张伟哲 <704052130@qq.com>
2021-07-26 17:37:02 +0800

    1、函数引用，把一个具名函数作为参数传递给另一个函数 2、返回类型为函数的函数

---
+++ src/kotlin02/PassFuncReference.kt
@@ -0,0 +1,28 @@
+package kotlin02
+
+fun main() {
+    // 函数引用，把一个具名函数作为实参传递给另一个函数，使用::
+    doPrint(::getTargetString)
+
+    val returnFunVal = returnFun()
+    println(returnFunVal("Bob", 10))
+}
+
+fun getTargetString():String {
+    return "hello word"
+}
+
+fun doPrint(getTargetString: ()->String) {
+    println(getTargetString())
+}
+
+// 返回类型为函数的函数
+// 没有入参，返回值类型为函数
+// 返回的函数，入参是(String, Int)，返回值类型是String
+fun returnFun(): (String, Int) -> String {
+    val s1 = "你好"
+    val num = 25
+    return { name:String, age:Int ->
+        "Name:$name s1:$s1 age:$age num:$num"
+    }
+}
