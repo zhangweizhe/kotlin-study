1b6ceac30913487bf3afaa0d204ddc8ffad314ca
张伟哲 <704052130@qq.com>
2021-10-28 10:07:11 +0800

    414. 第三大的数，快速排序解法

--- leetcode/src/sort/Q414.kt
+++ leetcode/src/sort/Q414.kt
@@ -3,7 +3,8 @@ package sort
 fun main() {
     // 414. 第三大的数
     // https://leetcode-cn.com/problems/third-maximum-number/
-    val intArrayOf = intArrayOf(1,2,-2147483648)
+    val intArrayOf = intArrayOf(1,1,1)
+
     println(thirdMax(intArrayOf))
 }
 
@@ -31,6 +32,29 @@ fun thirdMax(nums: IntArray): Int {
     }else {
         c.toInt()
     }
+
+}
+
+fun thirdMax1(nums: IntArray): Int {
+
+    val set:Set<Int> = HashSet<Int>(nums.toList())
+    val setNums = set.toIntArray()
+
+    if (setNums.size == 2) {
+        return if (setNums[0] > setNums[1]) {
+            setNums[0]
+        }else {
+            setNums[1]
+        }
+    }else if (setNums.size == 1) {
+        return setNums[0]
+    }
+
+    val quickSort = quickSort(setNums, 0, setNums.size - 1)
+    println("find index $quickSort")
+    println("nums ${setNums.contentToString()}")
+    println("find num ${setNums[quickSort]}")
+    return setNums[quickSort]
 }
 
 private fun quickSort(nums: IntArray, start: Int, end: Int):Int {
@@ -43,7 +67,7 @@ private fun quickSort(nums: IntArray, start: Int, end: Int):Int {
 
     var i = start
     for (j in start until end) {
-        if (nums[j] < pivot) {
+        if (nums[j] > pivot) {
             val tmp = nums[i]
             nums[i] = nums[j]
             nums[j] = tmp
@@ -52,14 +76,12 @@ private fun quickSort(nums: IntArray, start: Int, end: Int):Int {
     }
     nums[end] = nums[i]
     nums[i] = pivot
-    val leftCenter = quickSort(nums, start, i-1)
-    val rightCenter = quickSort(nums, i + 1, end)
-    if (i == nums.size - 3) {
+    if (i == 2) {
         println(pivot)
         return i
+    }else if (i > 2) {
+        return quickSort(nums, start, i-1)
+    }else {
+        return quickSort(nums, i+1, end)
     }
-    if (i > nums.size - 3) {
-        return leftCenter
-    }
-    return rightCenter
 }
